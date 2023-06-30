42094c00c68a82af9d6cdd8b6fdb662d440f7937
Vinit Kumar <vinit@apple.com>
2023-06-30 12:40:43 -0500

    LC solution

--- src/DP/targetSum/Partition_Equal_Subset_Sum_416.java
+++ src/DP/targetSum/Partition_Equal_Subset_Sum_416.java
@@ -1,4 +1,4 @@
-package DP;
+package DP.targetSum;
 
 import java.util.Arrays;
 import java.util.HashSet;
---
+++ src/DP/targetSum/Split_Array_With_Same_Average_805.java
@@ -0,0 +1,8 @@
+package DP.targetSum;
+
+public class Split_Array_With_Same_Average_805 {
+
+  public boolean splitArraySameAverage(int[] nums) {
+
+  }
+}
